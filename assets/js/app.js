97f67261299f3dc33122840d52752da6b6cfd243
Mohammod Ropan <ropan19gm@gmail.com>
2021-06-19 11:45:48 +0600

    new-file-upload

--- assets/js/app.js
+++ assets/js/app.js
@@ -83,6 +83,38 @@
     $(this).toggleClass('nav-link-show');
   });
 
+
+
+  /  light dark mode  /
+if (window.CSS && CSS.supports("color", "var(--primary)")) {
+  var toggleColorMode = function toggleColorMode(e) {
+    if (e.currentTarget.classList.contains("light--hidden")) {
+      document.documentElement.setAttribute("color-mode", "light"); 
+
+      localStorage.setItem("color-mode", "light");
+      return;
+    }
+    document.documentElement.setAttribute("color-mode", "dark"); 
+    localStorage.setItem("color-mode", "dark");
+  };
+  var toggleColorButtons = document.querySelectorAll(".color-mode__btn");
+  toggleColorButtons.forEach(function(btn) {
+    btn.addEventListener("click", toggleColorMode);
+  });
+} else {
+  var btnContainer = document.querySelector(".color-mode__header");
+  btnContainer.style.display = "none";
+}
+/  Match color to Dark  /
+
+if (
+  localStorage.getItem("color-mode") === "dark" ||
+  (window.matchMedia("(prefers-color-scheme: dark)").matches &&
+    !localStorage.getItem("color-mode"))
+) {
+  document.documentElement.setAttribute("color-mode", "dark");
+}
+
 }(jQuery));
   
 
